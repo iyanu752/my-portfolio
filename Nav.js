af14d6cdeffa8c3a83a596c73a72db4d1b9b4ec1
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2023-06-29 07:25:47 +0100

    Add files via upload
    navbar changes colour on scroll

---
+++ Nav.js
@@ -0,0 +1,63 @@
+import { useState, useEffect} from "react";
+import React from "react";
+import Logo from "../logo.png";
+import gitlogo from "../icons8-github-64.png";
+import linkedinlogo from "../icons8-linkedin-64.png";
+
+function Nav() {
+    const [navBar, setNavBar] = useState(false);
+
+
+    const changeBackground = () => {
+        if( window.scrollY >= 60) {
+            setNavBar(true);
+        }else {
+            setNavBar(false);
+        }
+
+    };
+
+    useEffect = ( () => {
+        window.addEventListener('scroll', changeBackground);
+
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        };
+
+    }, [window.scrollY]);
+
+
+       
+
+
+   
+    return(
+
+        <nav className={navBar ? "nav-bar active" : "nav-bar" }>
+            <div className="flex-div">
+            <div className="logo"><img className="logo" src= {Logo} /></div>
+            <div className="list-items">
+            <ul>
+                <a className="nav-el" href="#Home"><li>Home</li></a>
+                <a className="nav-el"  href="Skills"><li>Skills</li></a>
+                <a className="nav-el" href="#Projects"><li>Projects</li></a>
+                </ul>
+            </div>
+                
+                <div className="social-icons">
+                    <ul>
+
+                    
+                        <a href="#Github"><li className="social-list"> <img className="social-logo" src = {gitlogo} /></li></a>
+
+
+                        <a href="#LinkedIn"> <li className="social-list"><img className="social-logo" src = {linkedinlogo} /></li></a>
+                    </ul>  
+                    
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
