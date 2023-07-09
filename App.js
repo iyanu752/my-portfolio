1140d7bc3324e5ddf36d55e779baa46015b9d780
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2023-07-09 01:00:11 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import Skills from './components/skills';
+import Tools from './components/Tools';
+import Projects from './components/projects';
+import Contact from './components/contact';
+
+
+
 
 
 function App() {
   return (
     <div className="App">
+  
+
     <Nav />
     <Main />
+    <Skills/>
+    <Tools />
+    <Projects />
+    <Contact />
 
+     
     </div>
+
   );
 }
 
